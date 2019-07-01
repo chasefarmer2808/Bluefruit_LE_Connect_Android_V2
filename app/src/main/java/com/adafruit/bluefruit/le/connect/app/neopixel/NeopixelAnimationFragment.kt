0838c70e5099acd2342064b3dd333d6f83143dfa
chasefarmer2808 <chasefarmer2808@ufl.edu>
2019-06-30 22:33:21 -0400

    list view in anim fragment

--- app/src/main/java/com/adafruit/bluefruit/le/connect/app/neopixel/NeopixelAnimationFragment.kt
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/app/neopixel/NeopixelAnimationFragment.kt
@@ -3,9 +3,12 @@ package com.adafruit.bluefruit.le.connect.app.neopixel
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.ListFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 import com.adafruit.bluefruit.le.connect.R
 
@@ -22,9 +25,13 @@ class NeopixelAnimationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_neopixel_animation, container, false)
+
+        val rootView: View = inflater.inflate(R.layout.fragment_neopixel_animation, container, false)
+
+        val animListAdapter = ArrayAdapter<String>(activity, R.layout.animation_list_item, arrayOf("test"))
+        val animList: ListView = rootView.findViewById(R.id.animation_list)
+        animList.adapter = animListAdapter
+
+        return rootView
     }
-
-
 }
