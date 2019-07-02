093c1f93988289244ab9f806720f0d04453c6ad7
chasefarmer2808 <chasefarmer2808@ufl.edu>
2019-07-01 23:04:49 -0400

    my first coroutine

--- app/src/main/java/com/adafruit/bluefruit/le/connect/app/neopixel/NeopixelAnimationFragment.kt
+++ app/src/main/java/com/adafruit/bluefruit/le/connect/app/neopixel/NeopixelAnimationFragment.kt
@@ -3,16 +3,21 @@ package com.adafruit.bluefruit.le.connect.app.neopixel
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.app.ListFragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.coroutines.launch
 
 import com.adafruit.bluefruit.le.connect.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 
 class NeopixelAnimationFragment : Fragment() {
+
+    private var mFlag: Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -23,6 +28,25 @@ class NeopixelAnimationFragment : Fragment() {
         val animList: ListView = rootView.findViewById(R.id.animation_list)
         animList.adapter = animListAdapter
 
+        animList.setOnItemClickListener {_,_,position,_ ->
+            if (!mFlag) {
+                mFlag = true
+
+                GlobalScope.launch  {
+                    printTest()
+                }
+            }
+            else {
+                mFlag = false
+            }
+        }
+
         return rootView
+    }
+
+    private suspend fun printTest() {
+        while (mFlag) {
+            Log.d("TEST", "here")
+        }
     }
 }
